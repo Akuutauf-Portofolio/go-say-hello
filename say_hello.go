656fd6c148322da05182e549b68f45994fc6a21d
akuutauf <taufikhidayatbwx09121@gmail.com>
2025-11-20 10:27:27 +0700

    Major upgrade, changin say hello function to v2.0.0

--- say_hello.go
+++ say_hello.go
@@ -9,6 +9,15 @@ package go_say_hello
 // bisa dilakukan dengan melakukan perubahan terlebih dahulu
 // kemudian dilakukan commit, dan melakukan push tag yang baru
 
-func SayHello() string {
-	return "Hello World"
+// major upgrade
+// biasanya dilakukan ketika terjadi upgrade modules yang besar
+// hindari tindakan upgrade major dengan nama modules yang sama
+// sehingga nanti cukup di ubah saja, semisal mau upgrade major ke versi diatasnya yaitu v2.0.0
+// maka nama modules di project cukup diubah seperti ini :
+// github.com/Akuutauf-Portofolio/go-say-hello/v2
+// supaya nanti orang yang masih menggunakan versi yang sebelum upgrade major masih bisa mengunakannya
+// tujuannya agar tidak terjadi backward compatible: yang berpengaruh ke kode lainnya
+
+func SayHello(name string) string {
+	return "Hello " + name
 }
