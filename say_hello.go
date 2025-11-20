1b8457288f7daecfa4c5963db6758f75e0018870
akuutauf <taufikhidayatbwx09121@gmail.com>
2025-11-20 09:37:37 +0700

    adding comment

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,10 @@
 package go_say_hello
 
+// go modules
+// gunakan v untuk membuat tag di terminal git bash. contoh : v1.0.0
+// cara membuat tag : git tag v1.0.0
+// kemudian lakukan push : git push origin v1.0.0
+
 func SayHello() string {
 	return "Hello"
 }
